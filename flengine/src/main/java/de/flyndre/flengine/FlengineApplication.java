3b22c36fe68473e334c46c0625571ccd3354d680
L4kiLuk <49391930+L4kiLuk@users.noreply.github.com>
2023-10-20 16:03:46 +0200

    init java project

---
+++ flengine/src/main/java/de/flyndre/flengine/FlengineApplication.java
@@ -0,0 +1,13 @@
+package de.flyndre.flengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlengineApplication.class, args);
+	}
+
+}
---
+++ flengine/src/test/java/de/flyndre/flengine/FlengineApplicationTests.java
@@ -0,0 +1,13 @@
+package de.flyndre.flengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
