109cee862b1dc6c5e16191de6f761d5c7dfa4479
L4kiLuk <49391930+L4kiLuk@users.noreply.github.com>
2023-11-03 19:19:35 +0100

    FLE-56: add playmove() to Board.java

--- flengine/src/main/java/de/flyndre/flengine/datamodel/Board.java
+++ flengine/src/main/java/de/flyndre/flengine/datamodel/Board.java
@@ -1,5 +1,7 @@
 package de.flyndre.flengine.datamodel;
 
+import de.flyndre.flengine.datamodel.enums.Color;
+
 /**
  * @author Lukas
  * This class represents the chess board in the game logic.
@@ -10,6 +12,29 @@ public class Board {
      * First line second row
      */
     private Piece[][] pieces = new Piece[8][8];
+    /**
+     * Stores the color of the next on turn player.
+     */
+    private Color nextColor = Color.WHITE;
+
+    /**
+     * Executes a Move on the Board. If the target field is occupied it'll replace the piece on the target field with
+     * the piece on the source field. It also changes the color of the next player.
+     * @param move the move to be executed.
+     * @throws IllegalArgumentException if the from field og the move is empty.
+     */
+    public void playMove(Move move){
+        if (getPiece(move.getFrom())==null){
+            throw new IllegalArgumentException(String.format("The from field is empty. From field: %s",move.getFrom()));
+        }
+        setPiece(getPiece(move.getFrom()),move.getTo());
+        setPiece(null,move.getFrom());
+        if(this.nextColor==Color.BLACK){
+            this.nextColor = Color.WHITE;
+        }else {
+            this.nextColor = Color.BLACK;
+        }
+    }
 
     /**
      * Returns the piece from the given field
@@ -27,5 +52,20 @@ public class Board {
      */
     public void setPiece(Piece piece, Field field) {
         pieces[field.getLine().ordinal()][field.getRow().ordinal()] = piece;
+    }
+
+    /**
+     * @return the color of the next player in turn.
+     */
+    public Color getNextColor() {
+        return nextColor;
+    }
+
+    /**
+     * Sets the color of the next player in turn.
+     * @param nextColor the color of the next player.
+     */
+    public void setNextColor(Color nextColor) {
+        this.nextColor = nextColor;
     }
 }
