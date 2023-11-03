d7416d294ae4ff69d5d3e1636c14ce7ebe1584f9
L4kiLuk <49391930+L4kiLuk@users.noreply.github.com>
2023-11-03 18:58:53 +0100

    FLE-54: create Options.java

---
+++ flengine/src/main/java/de/flyndre/flengine/datamodel/Options.java
@@ -0,0 +1,9 @@
+package de.flyndre.flengine.datamodel;
+
+/**
+ * This class provides a data model to store options selected over UCI commands.
+ * It's currently not implemented and only there to be build in the functions to add later these options.
+ * @author Lukas
+ */
+public class Options {
+}
