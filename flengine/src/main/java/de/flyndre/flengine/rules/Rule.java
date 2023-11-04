7a1b328c6fff60b2205a4f4a75851969c3d1e3d8
tobitherobot <baumann.tobias@outlook.com>
2023-11-04 15:53:00 +0100

    added method structure

--- flengine/src/main/java/de/flyndre/flengine/rules/Rule.java
+++ flengine/src/main/java/de/flyndre/flengine/rules/Rule.java
@@ -1,4 +1,24 @@
 package de.flyndre.flengine.rules;
 
-public class Rule {
+import de.flyndre.flengine.datamodel.Board;
+import de.flyndre.flengine.datamodel.Field;
+import de.flyndre.flengine.datamodel.Move;
+import de.flyndre.flengine.datamodel.enums.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rule extends PieceRule {
+
+    public List<Move> getLegalMoves(Board board, Color color) { return new ArrayList<>(); }
+
+    public List<Move> getLegalMoves(Board board, Field field) { return new ArrayList<>(); }
+
+    public boolean isLegalMove(Board board, Move move) { return false; }
+
+    public boolean isCheck(Board board, Color color) { return false; }
+
+    public boolean isCheckmate(Board board, Color color) { return false; }
+
+    public boolean isRemis(Board board) { return false; }
 }
