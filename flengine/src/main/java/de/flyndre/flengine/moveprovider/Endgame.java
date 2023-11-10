fa23d8dc7ef3f4f70cffd7fd8140fae1f2b40759
dasdawidt <David_Maier@gmx.de>
2023-11-10 23:25:40 +0100

    FLE-83: implemented static endgame move provider

--- flengine/src/main/java/de/flyndre/flengine/moveprovider/Endgame.java
+++ flengine/src/main/java/de/flyndre/flengine/moveprovider/Endgame.java
@@ -1,12 +1,75 @@
 package de.flyndre.flengine.moveprovider;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import de.flyndre.flengine.converter.Converter;
 import de.flyndre.flengine.datamodel.Board;
 import de.flyndre.flengine.datamodel.Move;
+import de.flyndre.flengine.util.FileLogger;
+import javax.json.bind.JsonbBuilder;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
+/**
+ * An implementation of {@code MoveProvider} that uses the Lichess Tablebase
+ * to find the best move in an endgame situation.
+ * @author David
+ */
 public class Endgame implements MoveProvider {
+
+    public static class EndgameResponse {
+        public boolean checkmate;
+        public boolean stalemate;
+        public boolean variantWin;
+        public boolean variantLoss;
+        public boolean insufficientMaterial;
+        public EndgameCategory category;
+        public List<EndgameMove> moves;
+
+        public enum EndgameCategory {
+            WIN,
+            LOSS
+        }
+
+        public static class EndgameMove {
+            public String uci;
+            public boolean zeroing;
+            public boolean checkmate;
+            public boolean stalemate;
+            public boolean variantWin;
+            public boolean variantLoss;
+            public boolean insufficientMaterial;
+            public EndgameCategory category;
+        }
+    }
+
+    private final Logger logger = FileLogger.getLogger("Endgame");
+
+    /**
+     * Provides a list of recommended {@code Move}s for the given situation on the {@code Board}.
+     * @param board The current board.
+     * @return A list of recommended moves in this situation which may be empty if none were found.
+     */
     @Override
     public List<Move> getRecommendedMoves(Board board) {
-        return null;
+        // TODO: replace with FEN creation method of Converter
+        var fenString = "4k1K1/7P/8/8/8/8/7p/8 b - - 0 1";
+        var request = new Request.Builder()
+                .url("https://tablebase.lichess.ovh/standard?fen=" + fenString)
+                .build();
+        try {
+            logger.info("Requesting endgame moves for: [" + fenString + "]");
+            var response = new OkHttpClient().newCall(request).execute();
+            if (!response.isSuccessful()) throw new IOException("Unexpected response code: " + response);
+            var endgameData = JsonbBuilder.create().fromJson(response.body().string(), EndgameResponse.class);
+            logger.info("Received: [" + endgameData.moves.size() + " moves]");
+            return endgameData.moves.stream().map(m -> Converter.convertStringToMove(m.uci)).collect(Collectors.toList());
+        } catch (IOException e) {
+            logger.warning("Request failed: " + e.getMessage());
+            return new ArrayList<>();
+        }
     }
 }
