4c7993229fa7a9c613a665dcac67aca5f6301b6c
dasdawidt <David_Maier@gmx.de>
2023-11-10 20:31:08 +0100

    docs: added javadocs to MoveProvider

--- flengine/src/main/java/de/flyndre/flengine/moveprovider/MoveProvider.java
+++ flengine/src/main/java/de/flyndre/flengine/moveprovider/MoveProvider.java
@@ -2,9 +2,17 @@ package de.flyndre.flengine.moveprovider;
 
 import de.flyndre.flengine.datamodel.Board;
 import de.flyndre.flengine.datamodel.Move;
-
 import java.util.List;
 
+/**
+ * Provides functionality to obtain recommended moves for a given chess situation.
+ * @author David
+ */
 public interface MoveProvider {
+    /**
+     * Provides a list of recommended {@code Move}s for the given situation on the {@code Board}.
+     * @param board The current board.
+     * @return A list of recommended moves in this situation which may be empty if none were found.
+     */
     List<Move> getRecommendedMoves(Board board);
 }
