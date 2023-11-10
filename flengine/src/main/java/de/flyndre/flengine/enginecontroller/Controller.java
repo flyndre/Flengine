5245bb729d26ce7311977b185e4e01e54c1dac05
dasdawidt <David_Maier@gmx.de>
2023-11-10 23:48:24 +0100

    feat: added logging to Controller

--- flengine/src/main/java/de/flyndre/flengine/enginecontroller/Controller.java
+++ flengine/src/main/java/de/flyndre/flengine/enginecontroller/Controller.java
@@ -3,11 +3,15 @@ package de.flyndre.flengine.enginecontroller;
 import de.flyndre.flengine.datamodel.Board;
 import de.flyndre.flengine.datamodel.Move;
 import de.flyndre.flengine.datamodel.Options;
+import de.flyndre.flengine.moveprovider.Endgame;
 import de.flyndre.flengine.moveprovider.MoveProvider;
+import de.flyndre.flengine.moveprovider.Openings;
 import de.flyndre.flengine.moveprovider.Stub;
 import de.flyndre.flengine.rules.Rule;
+import de.flyndre.flengine.util.FileLogger;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * The centerpiece of Flengine.
@@ -16,9 +20,12 @@ import java.util.List;
  */
 public class Controller {
 
-    private final static List<MoveProvider> moveProviderHierarchy = Arrays.asList(
+    private static final Logger logger = FileLogger.getLogger("Controller");
+
+    private static final List<MoveProvider> moveProviderHierarchy = Arrays.asList(
+            new Openings(),
+            new Endgame(),
             new Stub(new Rule())
-            /* Insert other move providers here. */
     );
 
     /**
@@ -29,9 +36,14 @@ public class Controller {
      */
     public static Move giveMove(Board board, Options options) {
         for (var moveProvider : moveProviderHierarchy) {
+            logger.info("Requesting moves from: [" + moveProvider.getClass().getName() + "]");
             var moves = moveProvider.getRecommendedMoves(board);
-            if (moves != null && !moves.isEmpty()) return moves.get((int) Math.floor(Math.random() * moves.size()));
+            if (moves != null && !moves.isEmpty()){
+                logger.info("Received: [" + moves.size() + " moves]");
+                return moves.get((int) Math.floor(Math.random() * moves.size()));
+            }
         }
+        logger.warning("No possible moves were found.");
         return null;
     }
 }
