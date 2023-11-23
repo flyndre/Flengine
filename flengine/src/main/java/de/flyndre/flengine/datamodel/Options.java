351d9598bef33e719ae7bbc325745b636a120cea
dasdawidt <David_Maier@gmx.de>
2023-11-23 19:53:23 +0100

    FLE-137: use difficulty from options

--- flengine/src/main/java/de/flyndre/flengine/datamodel/Options.java
+++ flengine/src/main/java/de/flyndre/flengine/datamodel/Options.java
@@ -4,16 +4,17 @@ import de.flyndre.flengine.datamodel.enums.optionenums.EngineDifficulty;
 
 /**
  * This class provides a data model to store options selected over UCI commands.
- * It's currently not implemented and only there to be build in the functions to add later these options.
+ * It's currently not implemented and only there to be built in the functions to add later these options.
  * @author Lukas
  */
 public class Options {
 
     /**
-     * Represents the difficulty of the engine.
-     * Values are specified in the enum EngineDifficulty ranging from 1 (easiest) to 100 (hardest).
+     * Used to tweak the difficulty of the engine.
+     * Values are specified in the enum {@code EngineDifficulty}.
      */
     private EngineDifficulty engineDifficulty = EngineDifficulty.Normal;
+
     /**
      * Specifies whether UCI Debug Mode is activated.
      * DOES NOT NEED TO BE SPECIFIED BY GUI WITH OPTION COMMAND!
--- flengine/src/main/java/de/flyndre/flengine/datamodel/enums/optionenums/EngineDifficulty.java
+++ flengine/src/main/java/de/flyndre/flengine/datamodel/enums/optionenums/EngineDifficulty.java
@@ -1,5 +1,11 @@
 package de.flyndre.flengine.datamodel.enums.optionenums;
 
+/**
+ * Used to tweak the engine's difficulty.
+ * A lower value makes it less difficult, a higher one more difficult.
+ * The value 1 gives all moves the same probability.
+ * @author Paul
+ */
 public enum EngineDifficulty {
     Easy(1),
     Normal(8),
