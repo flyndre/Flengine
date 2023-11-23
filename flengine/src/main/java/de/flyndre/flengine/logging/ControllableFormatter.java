f32944e34143fca1922eff19777b52b55d6b27a3
dasdawidt <David_Maier@gmx.de>
2023-11-24 00:48:34 +0100

    FLE-134: create controllable formatter

---
+++ flengine/src/main/java/de/flyndre/flengine/logging/ControllableFormatter.java
@@ -0,0 +1,48 @@
+package de.flyndre.flengine.logging;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.logging.LogRecord;
+import java.util.logging.SimpleFormatter;
+
+public class ControllableFormatter extends SimpleFormatter {
+    private final String format;
+
+    public ControllableFormatter(String format) {
+        this.format = format;
+    }
+
+    @Override
+    public String format(LogRecord record) {
+        ZonedDateTime zdt = ZonedDateTime.ofInstant(
+                record.getInstant(), ZoneId.systemDefault());
+        String source;
+        if (record.getSourceClassName() != null) {
+            source = record.getSourceClassName();
+            if (record.getSourceMethodName() != null) {
+                source += " " + record.getSourceMethodName();
+            }
+        } else {
+            source = record.getLoggerName();
+        }
+        String message = formatMessage(record);
+        String throwable = "";
+        if (record.getThrown() != null) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            pw.println();
+            record.getThrown().printStackTrace(pw);
+            pw.close();
+            throwable = sw.toString();
+        }
+        return String.format(format,
+                zdt,
+                source,
+                record.getLoggerName(),
+                record.getLevel().getLocalizedName(),
+                message,
+                throwable);
+    }
+}
