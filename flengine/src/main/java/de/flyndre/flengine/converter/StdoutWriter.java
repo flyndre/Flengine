730e3212e2aa3cb23442b22444b18ebf4c325e6e
dasdawidt <David_Maier@gmx.de>
2023-12-14 18:15:28 +0100

    FLE-150: clean up javadoc

--- flengine/src/main/java/de/flyndre/flengine/converter/StdoutWriter.java
+++ flengine/src/main/java/de/flyndre/flengine/converter/StdoutWriter.java
@@ -1,15 +1,13 @@
 package de.flyndre.flengine.converter;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-
 /**
- * Provides a threadsafe possibility to write to stdout
+ * Provides a thread safe possibility to write to stdout.
+ * @author Paul
  */
 public class StdoutWriter {
 
     /**
-     * Prints the given string to the standard output
+     * Prints the given string to the standard output.
      * @param message the message to be printed to stdout
      */
     public static synchronized void writeToStdout(String message){
